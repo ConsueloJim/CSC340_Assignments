071d5cad0a3d42a444d3c6375341200c37e167a6
Consuelo Jimenez <jimenez.m.consuelo@gmail.com>
2015-02-06 09:25:52 -0800

    Initial Commit

---
+++ CSC340.HM1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CSC340.HM1
+//
+//  Created by Consuelo Jimenez on 2/6/15.
+//  Copyright (c) 2015 Consuelo Jimenez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
